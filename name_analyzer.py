ecb05e9b34930c2f32547b38556eefcaf195f928
M.Sathveekan <167169601+SathveekanM@users.noreply.github.com>
2025-06-19 17:16:22 +0530

    Create name_analyzer.py
    I created this project to understand what type of number it is, and it takes input from the user

---
+++ name_analyzer.py
@@ -0,0 +1,11 @@
+x = int(input("Enter any number: "))
+if x > 0:
+    print("It is a positive number")
+elif x < 0:
+    print("It is a negative number")
+else:
+    print("It is a zero number")
+if x % 2 == 0:
+    print("It is even")
+else:
+    print("It is odd")
